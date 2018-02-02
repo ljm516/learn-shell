e0da5a31d40b5bd7beb1fffe48a003cc0cfc6862
ljm516 <605057476@qq.com>
2018-02-02 16:33:32 +0800

    Update test_db_dump.sh

--- db_handler/test_db_dump.sh
+++ db_handler/test_db_dump.sh
@@ -7,15 +7,15 @@
 # fi
 
 # ENV=$1
-SOURCE_HOST=rm-2zeelpc79uk360qhk.mysql.rds.aliyuncs.com
-SOURCE_USER=prd_read_account
-SOURCE_PASSWD=iRoqcCwhT6v8DC
+SOURCE_HOST='host'
+SOURCE_USER='usename'
+SOURCE_PASSWD='password'
 TARGET_HOST=127.0.0.1
 TARGET_USER=root
 TARGET_PASSWD=''
 
 
-declare -a databases=(civilization dagon hdfragment hibernation kangfubao mentalseal nsk oldyolar owl singer solar sophon yolar)
+declare -a databases=(......)
 
 for db in "${databases[@]}"
 do
@@ -34,6 +34,4 @@ do
     mysql --host=$TARGET_HOST --user=$TARGET_USER --password=$TARGET_PASSWD $TARGET_DB < $DUMP_FILE
     printf "Restore Finished\n"
 done
-
-python /Users/jiangmingli/youhujia/wk/magic-era/yolar_wechat_profile/recover_open_id.py
 
